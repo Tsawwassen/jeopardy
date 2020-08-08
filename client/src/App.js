512606ae4f49cbf95ab0e533d301869f89f6ec79
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-08 08:24:39 -0700

    Add bootstrap to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Navbar from './components/navbar/navbar.js'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-     <div className="App"> <h1>hello</h1> </div>
+     <div className="App"> 
+      <Navbar />
+      <div className="container">
+        <h1>hello</h1>
+      </div>
+
+     </div>
   );
 }
 
