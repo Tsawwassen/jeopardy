d3b341b6290dac379130d72cf00e04e58e66c76e
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-08 16:02:09 -0700

    Minor change

--- client/src/components/Jeopardy/home.js
+++ client/src/components/Jeopardy/home.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import {Button} from 'react-bootstrap';
 
 class Home extends Component {
-
-
-  render () { 
-  	return (
-  		<div>
-  			<Button onClick={this.props.onClick}> Create Jeopardy Game </Button>
-  		</div>
+  	render () { 
+  		return (
+  			<div>
+  				<Button onClick={this.props.onClick}> Create Jeopardy Game </Button>
+  			</div>
   		);
-  	
-  }
+  	}
 }
 
 export default Home;
