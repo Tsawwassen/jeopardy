9006870c51d4e4d83dcc0c4e501f7f0dccdb6d1f
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-09 16:57:56 -0700

    Add answer component to the porject

---
+++ client/src/components/Jeopardy/answer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
+
+class Answer extends Component {
+
+  	render () { 
+  		console.log("Inside answer component");
+  		return (
+  			<div>
+  				<h1>Answer</h1>
+  			
+  			</div>
+  		);
+  	}
+}
+
+export default Answer;
